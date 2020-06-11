3f7cabfa7942854ec65404eaee60441634d742d4
August Bolter <august.bolter@gmail.com>
2020-06-11 20:50:16 +1200

    Added LogMonitor (Swing UI for logs). LogMonitor only at the moment has a basic structure and only has just been started.
    Signed-off-by: August Bolter <august.bolter@gmail.com>

---
+++ src/main/java/nz/ac/vuw/swen301/a2/client/LogMonitor.java
@@ -0,0 +1,11 @@
+package nz.ac.vuw.swen301.a2.client;
+
+import javax.swing.*;
+
+public class LogMonitor {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.add(panel);
+    }
+}
