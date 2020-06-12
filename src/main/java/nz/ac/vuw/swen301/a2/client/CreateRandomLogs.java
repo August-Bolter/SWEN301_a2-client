68ce8a4dab81e5b947af55764ddc6f7f6d112132
August Bolter <august.bolter@gmail.com>
2020-06-12 16:08:50 +1200

    Added more code comments to CreateRandomLogs and Resthome4LogsAppender
    Signed-off-by: August Bolter <august.bolter@gmail.com>

--- src/main/java/nz/ac/vuw/swen301/a2/client/CreateRandomLogs.java
+++ src/main/java/nz/ac/vuw/swen301/a2/client/CreateRandomLogs.java
@@ -8,18 +8,22 @@ import org.apache.log4j.spi.ThrowableInformation;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
+/** Creates random logs once per second. ALl aspects of the log are random (or pseudo random) */
 public class CreateRandomLogs {
 
     public static void main(String[] args) throws InterruptedException {
-        Resthome4LogsAppender appender = new Resthome4LogsAppender();
+        Resthome4LogsAppender appender = new Resthome4LogsAppender(); //Creating the appender
 
+        /* Possible options for the log aspects. One option for each aspect will be chosen randomly */
         String[] loggerNames = {"foo", "bar", "baz", "rez", "pez"};
         String[] messageNames = {"This is a log", "This log contains info", "Something has happened", "This log is useful"};
         String[] threadNames = {"main", "daemon", "concurrent"};
         String[] throwableInfoNames = {"An exception has occurred", "Everything is working well", "Something has gone wrong", "An issue has occurred"};
 
+        /* Make logs continuously (forever) */
         while (true) {
             Random rand = new Random();
+            /* Assigning a random level */
             int randomNum = rand.nextInt(8);
             Level level = null;
             switch(randomNum) {
@@ -47,21 +51,22 @@ public class CreateRandomLogs {
                 case 7 :
                     level = Level.ALL;
             }
-            LoggingEvent event;
-            long bound = 1591963200L*1000L;
-            int errorDetailsPresent = rand.nextInt(2);
-            if (errorDetailsPresent == 0) {
+            LoggingEvent event; //The log that will be sent to the appender
+            long bound = 1591963200L*1000L; //This is the maximum value for timestamp that can be randomly generated. This value is the difference in miliseconds between 1970-01-01 and the 12th June 2020
+            int errorDetailsPresent = rand.nextInt(2); //Decides whether log will have errorDetails or not
+            if (errorDetailsPresent == 0) { //Log will have errorDetails
+                /* Pick errorDetails message randomly */
                 String[] info = new String[1];
                 info[0] = throwableInfoNames[rand.nextInt(4)];
-                ThrowableInformation throwable = new ThrowableInformation(info);
-                Logger logger = Logger.getLogger(loggerNames[rand.nextInt(5)]);
-                event = new LoggingEvent(logger.getName(), logger, ThreadLocalRandom.current().nextLong(bound), level, messageNames[rand.nextInt(4)], threadNames[rand.nextInt(3)], throwable, null, null, null);
+                ThrowableInformation throwable = new ThrowableInformation(info); //Creating ThrowableInformation which contains this message
+                Logger logger = Logger.getLogger(loggerNames[rand.nextInt(5)]); //Creating a logger with a random logger name
+                event = new LoggingEvent(logger.getName(), logger, ThreadLocalRandom.current().nextLong(bound), level, messageNames[rand.nextInt(4)], threadNames[rand.nextInt(3)], throwable, null, null, null); //Creating the log
             }
-            else {
-                event = new LoggingEvent("foo", Logger.getLogger(JsonObject.class), ThreadLocalRandom.current().nextLong(bound), level, messageNames[rand.nextInt(4)], threadNames[rand.nextInt(3)], null, null, null, null);
+            else { //Log will not have errorDetails
+                event = new LoggingEvent("foo", Logger.getLogger(JsonObject.class), ThreadLocalRandom.current().nextLong(bound), level, messageNames[rand.nextInt(4)], threadNames[rand.nextInt(3)], null, null, null, null); //Creating the log
             }
-            appender.append(event);
-            Thread.sleep(1000);
+            appender.append(event); //Send logging event to appender
+            Thread.sleep(1000); //Wait 1 second
         }
     }
 }
--- src/main/java/nz/ac/vuw/swen301/a2/client/Resthome4LogsAppender.java
+++ src/main/java/nz/ac/vuw/swen301/a2/client/Resthome4LogsAppender.java
@@ -16,20 +16,26 @@ import java.net.URISyntaxException;
 import java.time.Instant;
 import java.util.UUID;
 
+/** This is a log4j appender that appends the log it receives to an Http Server. */
 public class Resthome4LogsAppender extends AppenderSkeleton {
 
+    /** Gets the Http Service URL
+     * @return The Http Service URL */
     public URI getLogServiceURL() {
         return logServiceURL;
     }
 
+    /** Sets the Http Service URL
+     * @param logServiceURL The HTTP Service URL */
     public void setLogServiceURL(URI logServiceURL) {
         this.logServiceURL = logServiceURL;
     }
 
-    private URI logServiceURL;
+    private URI logServiceURL; //The Http Service URL used for connecting the client to the server
 
+    /** Creates a Resthome4LogsAppender and it's logServiceURl */
     public Resthome4LogsAppender() {
-        PropertyConfigurator.configure("src/main/resources/log4j.properties");
+        PropertyConfigurator.configure("src/main/resources/log4j.properties"); //Configure appender to these properties
         URIBuilder builder = new URIBuilder();
         builder.setScheme("http").setHost("localhost").setPort(8080).setPath("/resthome4logs/logs");
         URI uri = null;
@@ -41,27 +47,31 @@ public class Resthome4LogsAppender extends AppenderSkeleton {
         setLogServiceURL(uri);
     }
 
+    /** Appends a log to the Http Server
+     * @param loggingEvent The log being appended */
     protected void append(LoggingEvent loggingEvent) {
-        if (loggingEvent != null) { //Accounting for null
+        if (loggingEvent != null) { //Avoiding NullPointerException
             JsonObject newObj = new JsonObject(); //Creating JSON object
-            /* Assigning important attributes like logger, level etc. to JSON object */
+            /* Assigning attributes obtained from log like logger, level etc. to JSON object */
             newObj.addProperty("id", UUID.randomUUID().toString());
             newObj.addProperty("message", loggingEvent.getRenderedMessage());
-            Instant time = Instant.ofEpochMilli(loggingEvent.timeStamp);
+            Instant time = Instant.ofEpochMilli(loggingEvent.timeStamp); //Calculates date based on 1970-01-01 + milliseconds (loggingEvent.timeStamp)
             newObj.addProperty("timestamp", time.toString());
             newObj.addProperty("thread", loggingEvent.getThreadName());
             newObj.addProperty("logger", loggingEvent.getFQNOfLoggerClass());
             newObj.addProperty("level", loggingEvent.getLevel().toString());
+            /* Assigning Throwable Information attribute obtained from log to JSON object */
             if (loggingEvent.getThrowableInformation() != null) {
                 String[] errorInfo = loggingEvent.getThrowableInformation().getThrowableStrRep();
                 newObj.addProperty("errorDetails", errorInfo[0]);
             }
+            /* Create the client and the post request */
             HttpClient httpClient = HttpClientBuilder.create().build();
             HttpPost postRequest = new HttpPost(logServiceURL);
             try {
-                postRequest.addHeader("Content-Type", "application/json");
-                postRequest.setEntity(new StringEntity(newObj.toString()));
-                httpClient.execute(postRequest);
+                postRequest.addHeader("Content-Type", "application/json"); //Adding content type header
+                postRequest.setEntity(new StringEntity(newObj.toString())); //Setting entity (body content) for the request
+                httpClient.execute(postRequest); //Send the request to the server using the httpClient
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -69,9 +79,12 @@ public class Resthome4LogsAppender extends AppenderSkeleton {
 
     }
 
+    /** Closes the appender so that for any other process trying to append using this appender, appending will not work */
     public void close() {
     }
 
+    /** Determines whether this appender needs a layout
+     * @return whether the appender needs a layout */
     public boolean requiresLayout() {
         return false;
     }
