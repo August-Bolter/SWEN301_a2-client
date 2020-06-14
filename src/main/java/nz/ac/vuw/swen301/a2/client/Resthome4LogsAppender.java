d79112202779448ba34fdac2a5f081601b89457f
August Bolter <august.bolter@gmail.com>
2020-06-14 21:59:49 +1200

    Changed logServiceURL to be a string instead of a URI and moved the code which creates the URI to the append method instead of in the constructor. Http Client now uses the URI instead of logServiceURL to create the client.
    Signed-off-by: August Bolter <august.bolter@gmail.com>

--- src/main/java/nz/ac/vuw/swen301/a2/client/Resthome4LogsAppender.java
+++ src/main/java/nz/ac/vuw/swen301/a2/client/Resthome4LogsAppender.java
@@ -19,32 +19,24 @@ import java.util.UUID;
 /** This is a log4j appender that appends the log it receives to an Http Server. */
 public class Resthome4LogsAppender extends AppenderSkeleton {
 
+    private String logServiceURL; //The Http Service URL used for connecting the client to the server
+
     /** Gets the Http Service URL
      * @return The Http Service URL */
-    public URI getLogServiceURL() {
+    public String getLogServiceURL() {
         return logServiceURL;
     }
 
     /** Sets the Http Service URL
      * @param logServiceURL The HTTP Service URL */
-    public void setLogServiceURL(URI logServiceURL) {
+    public void setLogServiceURL(String logServiceURL) {
         this.logServiceURL = logServiceURL;
     }
 
-    private URI logServiceURL; //The Http Service URL used for connecting the client to the server
-
     /** Creates a Resthome4LogsAppender and it's logServiceURl */
     public Resthome4LogsAppender() {
+        setLogServiceURL("http://localhost:8080/resthome4logs/logs"); //This is the default URL for the appender.
         PropertyConfigurator.configure("src/main/resources/log4j.properties"); //Configure appender to these properties
-        URIBuilder builder = new URIBuilder();
-        builder.setScheme("http").setHost("localhost").setPort(8080).setPath("/resthome4logs/logs");
-        URI uri = null;
-        try {
-            uri = builder.build();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        setLogServiceURL(uri);
     }
 
     /** Appends a log to the Http Server
@@ -65,9 +57,42 @@ public class Resthome4LogsAppender extends AppenderSkeleton {
                 String[] errorInfo = loggingEvent.getThrowableInformation().getThrowableStrRep();
                 newObj.addProperty("errorDetails", errorInfo[0]);
             }
+            /* Obtaining details from the URL to use to create the URI. */
+            URIBuilder builder = new URIBuilder();
+            StringBuilder portString = new StringBuilder(); //The port in the URL
+            StringBuilder path = new StringBuilder(); //The path in the URL
+            StringBuilder hostName = new StringBuilder(); //The host in the URL
+            boolean foundPort = false;
+            boolean startPath = false;
+            for (int i = 7; i < logServiceURL.length(); i++) { //Skipping http: in search
+                if (!foundPort && !startPath && logServiceURL.charAt(i) != ':') { //Host starts after http://
+                    hostName.append(logServiceURL.charAt(i)); //Storing the host
+                }
+                if (foundPort && logServiceURL.charAt(i) == '/') { //Path starts at 3rd / in the URL
+                    foundPort = false;
+                    startPath = true;
+                }
+                if (foundPort) {
+                    portString.append(logServiceURL.charAt(i)); //Storing the port
+                }
+                if (logServiceURL.charAt(i) == ':') { //Port starts at second : in the URL
+                    foundPort = true;
+                }
+                if (startPath) {
+                    path.append(logServiceURL.charAt(i)); //Storing the path
+                }
+            }
+            builder.setScheme(logServiceURL.substring(0, 4)).setHost(hostName.toString()).setPort(Integer.parseInt(portString.toString())).setPath(path.toString());
+            URI uri = null;
+            try {
+                uri = builder.build();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+
             /* Create the client and the post request */
             HttpClient httpClient = HttpClientBuilder.create().build();
-            HttpPost postRequest = new HttpPost(logServiceURL);
+            HttpPost postRequest = new HttpPost(uri);
             try {
                 postRequest.addHeader("Content-Type", "application/json"); //Adding content type header
                 postRequest.setEntity(new StringEntity(newObj.toString())); //Setting entity (body content) for the request
